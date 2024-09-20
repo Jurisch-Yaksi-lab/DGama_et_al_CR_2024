ce0efe32e8aa25ad9ed7ed4ca19709d610a81be9
nathalju <nathalie.jurisch-yaksi@ntnu.no>
2024-09-20 17:55:38 +0200

    update on 20.09.2024

--- 2photon GLIA data analysis/ReadME.txt
+++ 2photon GLIA data analysis/ReadME.txt
@@ -15,17 +15,17 @@ Following image acquisition, cerebral blood flow was investigated in the zebrafi
 
 # Data analysis
 
-All analysis were done using matlab.
+All analysis were done using matlab (matlab version 2023 works, or earlier).
 
 # STEP1 Image alignment
 Data were aligned using alignment tools described in Reiten et al, Current Biology, 2017, https://doi.org/10.1016/j.cub.2016.11.036
 
 Unstable recordings were discarded. One control recording was removed due to unexpected high activity, which was consider an outlier using the isoutlier test in matlab. One recording was removed due to lower fluorescence than average
-In total 11 control datasets and 12 mutant datasets were included in the analysis.
+In total 10 control datasets and 11 mutant datasets were included in the analysis.
 
 # STEP2 Identification of region of interest (ROI)
-ROI were segmented and assigned to brain regions using tools described in Bartoszek et al, Current Biology, 2021, https://doi.org/10.1016/j.cub.2021.08.021
-Large ROI were drawn along the ventricle lining in each brain region. Each ROI consisted of multiple cells.
+Large ROI were drawn along the ventricle lining in each brain region. 
+Each ROI consisted of multiple cells.
 
 All datasets are available on NIRD at doi indicated in the manuscript
  - Data for panel 
@@ -37,32 +37,29 @@ Analysis pipelines are as indicated below:
 Step 3.1: Calculate DFF
 Master files: "GLIA_analysis_perfish_v2", using function "plot_response_v4.m"
 This code does:
+	- Load the ROI data and traces
+	- Assign each ROI to a brain region, the assignement to brain regions was manually. 3 brain regions were chosen: forebrain (anterior to habenula), forebrain-midbrain (poster to and including habenula), hindbrain
 	- Input light onsets/offsets
 	- Calculate DFF for each stimulus using as baseline the 5s before each stimulus, and the average response per ROI.
-	- Calculate DFF for the entire recording (start at circa 2min /frame 460 to avoid artefacts due to laser turning on) according to Romano et al, Plos computational biology, 2017 https://doi.org/10.1371/journal.pcbi.1005526 (based on 8% percentile). Use a Decay of F0cfg.tauDecay=2;
+	- Calculate DFF for the entire recording according to Romano et al, Plos computational biology, 2017 https://doi.org/10.1371/journal.pcbi.1005526 (based on 8% percentile). Use a Decay of F0cfg.tauDecay=2;
 	- This code averages the ROI from the same brain regions and then all the ROIs
+
+F0cfg.start=460; % frame where to start the DFF calculation, this is equivalent to around 2min
+F0cfg.stop=9250; % frame where to stop the calculation, 2304 is slightly before LIGHTonset
 
 
 Step 3.2: compile datasets for all fish
 Responses across animals were calculated and plotted using code
-Fig4_GLIA_ANALYSIS.mlx
+Fig4_GLIA_ANALYSIS_v2.mlx, functions are nested in the live editor file
 
-UPDATE BELOW
+
 ===========================================
-Necessary codes to run the analysis:
-
-
-
-
 Necessary codes to run the analysis
-"LoadF0resample.m",
-"computationF0-LL.m",
-"clustering_LL_NJ2.m",
-"plot_clusteringSPONT_NJ.m",
-"nj_ClusterPerBrainRegion_Allfish.m",
-"nj_plotCellsThreshold.m",
-"nj_correlationVSdistance_v2.m",
-"binscatter.m"
+"GLIA_analysis_perfish_v2.m"
+"plot_response_v4.m",
+"computationF0_LL.m",
+"nj_load2pGliadata_v2.m"
+
 
 
 
---
+++ 2photon GLIA data analysis/pullingGliadatatogether_NewData.m
@@ -0,0 +1,102 @@
+% % ctrls (11)
+load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20221027_15_56_48_20221027GFAPgal4UASsmhctrlF02\Aligned\celldetection_manual\20221027GFAPgal4UASsmhctrlF02_Results_DV_nodoubles.mat')
+brainRegion=[3,3,2,2,3,3,2,2,1,1,2,2,3,3,3,3,1,1,2,2,3,3,2,2,1,1,1,1,2,2,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20221128_14_15_08_20221128GFAPgal4UASsmhctrlF02\Aligned\celldetection_manual\20221128GFAPgal4UASsmhctrlF02_Results_DV_nodoubles.mat')
+brainRegion=[3,3,3,3,2,2,2,2,3,3,1,1,3,3,2,2,3,3,1,1,3,3,2,2,1,1,1,1,2,2,3,3,3,3,2,2,1,1,1,1,2,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\2023_04_26\20230426_14_02_42_20230426GFAPgal4UASsmhctrlF04\Aligned\celldetection_manual\20230426GFAPgal4UASsmhctrlF04_Results_DV_nodoubles.mat')
+% brainRegion=[3,2,3,2,2,2,3,2,1,1,3,2,1,1,1,1,3,2,1,1,1,1,2,1,1,1,1,1,2]; % needed to remove the 30th point which was a OT because of a double ROI,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_21_39_56_20230718GFAPgal4UASsmctrlF05\Aligned\celldetection_manual\20230718GFAPgal4UASsmctrlF05_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,3,2,2,2,2,3,3,2,2,3,3,3,3,1,1,2,2,2,2,1,1,2,2,1,1,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_22_35_51_20230718GFAPgal4UASsmctrlF06\Aligned\celldetection_manual\20230718GFAPgal4UASsmctrlF06_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,2,2,2,2,3,3,2,2,3,3,1,1,2,2,3,3,2,2,3,3,1,1,3,3,2,2,1,1,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_09_25_40_20230718GFAPgal4UASsmhctrlF01\Aligned\celldetection_manual\20230718GFAPgal4UASsmhctrlF01_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,2,2,2,2,2,2,3,3,2,2,3,3,2,2,2,2,3,3,2,2,2,3,3,2,2,2,2,2,2,1,1,2,2,2,2,1,1,2,2,2];
+% [amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_17_23_29_20230718GFAPgal4UASsmhctrlF03\Aligned\celldetection_manual\20230718GFAPgal4UASsmhctrlF03_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,3,2,2,3,3,3,2,2,2,2,2,2,2,3,3,2,1,2,2,2,3,3,3,3,2,2,1,1,2,2,2,2,1,1,2,2,1,1,2,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_18_29_44_20230718GFAPgal4UASsmhctrlF04\Aligned\celldetection_manual\20230718GFAPgal4UASsmhctrlF04_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,2,2,2,2,2,2,3,3,2,2,2,3,3,1,1,2,2,2,1,1,2,2,1,1,2,2,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20240719_16_46_29_20240719GFAPgal4UASsmhctrlF03\Aligned\celldetection_manual\20240719GFAPgal4UASsmhctrlF03_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,2,2,2,2,2,3,3,1,1,2,2,3,3,3,3,2,2,1,1,3,3,3,3,2,2,1,1,1,1,2,2,3,3,3,3,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20240729_11_45_09_20240729GFAPgal4UASsmhctrlF01\Aligned\celldetection_manual\20240729GFAPgal4UASsmhctrlF01_Results_DV_nodoubles.mat')
+% brainRegion=[2,2,2,2,1,1,3,3,2,2,2,2,3,3,1,1,2,2,3,3,2,2,1,1,3,3,2,2,1,1,3,3,1,1,2,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20240729_13_16_13_20240729GFAPgal4UASsmhctrlF02\Aligned\celldetection_manual\20240729GFAPgal4UASsmhctrlF02_Results_DV_nodoubles.mat')
+% brainRegion=[2,2,2,2,2,2,3,3,1,1,2,2,2,2,3,3,1,1,2,2,3,3,1,1,2,2,1,1,2,2,3,3,1,1,2,2,3,3,2];
+[amplitude]=plot_response_v4 (results,path)
+
+% %mutants (11)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20221218_11_45_16_20221218GFAPgal4UASsmhmutF01\Aligned\celldetection_manual\20221218GFAPgal4UASsmhmutF01_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,2,2,2,3,3,2,2,2,2,2,3,3,3,3,2,2,2,2,1,1,2,2,2,2,2,2,3,3,3,2,2,2,2,1,1,2,2,1,1,2,2,2,2,1,2,2,1,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20221218_12_50_09_20221218GFAPgal4UASsmhmutF02\Aligned\celldetection_manual\20221218GFAPgal4UASsmhmutF02_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,3,3,2,2,3,3,2,2,3,3,3,3,2,2,2,2,2,2,3,3,3,3,2,2,2,1,1,2,2,2,2,1,1,2,2,1,1,2,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\2023_04_20\20230420_18_10_23_20230420GFAPgal4UASsmhmutF01\Aligned\celldetection_manual\20230420GFAPgal4UASsmhmutF01_Results_DV_nodoubles.mat')
+% brainRegion=[2,2,2 1,1,1,1,3,3,2,1,1,1,1,2,1,1,1,1,1,2,1,1,1,3,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\2023_04_26\20230426_13_08_57_20230426GFAPgal4UASsmhmutF01\Aligned\celldetection_manual\20230426GFAPgal4UASsmhmutF01_Results_DV_nodoubles.mat')
+% brainRegion=[2,2,2,2,1,1,3,1,1,2,3,3,1,1,1,1,2,2,1,1,1,3,3,2,1,1,1,1,1,1,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\2023_04_26\20230426_18_24_20_20230426GFAPgal4UASsmhmutF04\Aligned\celldetection_manual\20230426GFAPgal4UASsmhmutF04_Results_DV_nodoubles.mat')
+% brainRegion=[2,3,3,1,1,1,1,2,1,1,1,1,3,2,3,1,1,2,1,3,2,1,1,1,3,2,1,1,1,3,1,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_12_22_42_20230718GFAPgal4UASsmhmutF01\Aligned\celldetection_manual\20230718GFAPgal4UASsmhmutF01_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,3,3,2,2,3,3,3,3,2,2,2,2,3,3,3,3,2,2,3,3,2,2,1,1,2,2,1,1,2,2,2,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_19_40_43_20230718GFAPgal4UASsmhmutF04\Aligned\celldetection_manual\20230718GFAPgal4UASsmhmutF04_Results_DV_nodoubles.mat')
+% brainRegion=[1,1,2,2,1,1,2,2,3,3,2,2,1,1,3,3,2,2,1,1,3,3,2,2,1,1,3,3,2,2,1,1,3,3,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_20_40_49_20230718GFAPgal4UASsmhmutF05\Aligned\celldetection_manual\20230718GFAPgal4UASsmhmutF05_Results_DV_nodoubles.mat')
+% brainRegion=[2,2,3,3,1,1,2,2,2,2,2,1,1,2,3,3,2,2,3,3,1,1,3,3,2,2,1,1,3,3,2,2,1,1,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20230718_16_18_32_20230718GFAPgal4UASsmhmutlF03\Aligned\celldetection_manual\20230718GFAPgal4UASsmhmutlF03_Results_DV_nodoubles.mat')
+% brainRegion=[3,3,3,3,2,2,3,3,2,2,3,3,2,2,1,1,2,2,3,3,1,1,2,2,3,3,1,1,2,2,1,1,2,2,2];
+[amplitude]=plot_response_v4 (results,path)
+% load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20240719_11_47_41_20240719GFAPgal4UASsmhmutF01\Aligned\celldetection_manual\20240719GFAPgal4UASsmhmutF01_Results_DV_nodoubles.mat')
+% brainRegion=[2,2,1,1,2,2,3,3,2,2,2,2,1,1,2,2,1,1,3,3,2,2,1,1,2,2,1,1,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+load('X:\Percival\2photon data\glia 2p\smh\Glia 2p ROIs redone\20240729_10_36_01_20240729GFAPgal4UASsmhmutF01\Aligned\celldetection_manual\20240729GFAPgal4UASsmhmutF01_Results_DV_nodoubles.mat')
+brainRegion=[1,1,2,2,2,2,2,2,1,1,1,1,2,2,1,1,2,2,1,1,2,2,1,1,2,2,3,3,3,3,2,2,1,1,2];
+[amplitude]=plot_response_v4 (results,path)
+
+%%
+addpath(genpath('X:\Nathalie\data\code'))
+%% code to check that the brain region are well separated
+%% code to check that the brain region are well separated
+% it plots a subplot for each ROI which is color-coded based ont he brain
+% region
+clear mat
+figure,
+for k=1:length(brainRegion)
+    mat=squeeze(results.neuronLabels(:,:,:)==k);
+    % Find the indices of the slices that are entirely zero
+    zero_slices = all(all(mat == 0, 1), 2);
+    % Remove the zero slices
+    mat(:, :, zero_slices) = [];
+    mat=mat*brainRegion(k);
+    subplot(1,length(brainRegion),k)
+    imagesc(mat), caxis([0 4])
+    xlim([0 512])
+    ylim([0 1536])
+    axis off
+    colormap jet
+    %colorbar
+    clear mat
+
+end
+%%
+%%
+dex2(results.volume)
+
+dex2(results.neuronLabels)
+colormap jet
+caxis([0 23])
